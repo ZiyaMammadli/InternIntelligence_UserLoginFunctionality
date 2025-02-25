116d15aa01923342e9d2a40475fa48aeed2ebc40
Your Name <you@example.com>
2025-02-25 10:57:50 +0400

    CreatedITokenService

---
+++ Src/Core/UserLoginFunctionality.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UserLoginFunctionality.Domian.Entities;
+
+namespace UserLoginFunctionality.Application.Services;
+
+public interface ITokenService
+{
+    public Task<JwtSecurityToken> GenerateAccessTokenAsync(AppUser appUser,IList<Role> roles);
+    public string GenerateRefreshToken();
+    public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token);
+}
